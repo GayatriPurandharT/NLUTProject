f29597be74d6e16568836d5c3ad32f304ffcb679
GayatriPurandharT <st119978@ait.asia>
2019-07-17 15:55:28 +0700

    vk_adult_child_test.txt contains all lines, vk_adult_child_tokens is tokenized form

--- adult_child.py
+++ adult_child.py
@@ -1,20 +1,39 @@
-str1 = "*CHI"
-str2 = "*ADU"
+
+import nltk
+from nltk import word_tokenize
+
+str1 = "*CHI:"
+str2 = "*ADU:"
 mor = "%mor"
 i=0
 j=0
+delete_list = ['?',',','.','_','@c','@o','@b']
 with open("vk.txt") as file:
-    with open("vk_adult.txt", "w") as f1:
-        with open("vk_child.txt", "w") as f2:         
-            for line in file:
-                if str1 in line:
-                    f1.write(line)
-                elif mor in line:
-                                    
-                elif str2 in line:
-                    j=j+1
-                    f2.write(line)
-            
 
-print("Adult: "+str(i))
-print("Child: "+str(j))
+    with open("vk_adult_child_test.txt", "w") as f1:  
+        with open("vk_adult_child_tokens.txt","w") as f2:       
+            for line in file:
+
+                #extracting only adu and chi lines and removing adu chi tags
+                if str1 in line:
+                    line = line.replace(str1, "")
+                    f1.write(line)
+                    i = i+1       
+                    tokens_chi = nltk.word_tokenize(line)
+                    f2.write(str(tokens_chi))
+
+                elif str2 in line:
+                    line = line.replace(str2, "")
+                    f1.write(line)
+                    j=j+1
+                    tokens_adu = nltk.word_tokenize(line)
+                    f2.write(str(tokens_adu))
+            
+        
+                    
+
+
+
+print("Child: "+str(i))
+print("Adult: "+str(j))
+print(len(tokens_chi))
